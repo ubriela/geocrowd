f5adceb3a03bc9599cb6c637b36ad89e5752434a
unknown <tranvanluan7@gmail.com>
2014-06-21 17:11:31 +0700

    luan rewrite greedy wait till deadline

--- GeoCrowd/src/org/geocrowd/setcover/SetCoverGreedyWaitTillDeadline.java
+++ GeoCrowd/src/org/geocrowd/setcover/SetCoverGreedyWaitTillDeadline.java
@@ -6,6 +6,7 @@
 package org.geocrowd.setcover;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Objects;
 
@@ -15,79 +16,85 @@ import java.util.Objects;
  */
 public class SetCoverGreedyWaitTillDeadline {
 
-    ArrayList<HashSet<Integer>> setOfSets = null;
+    // hashmap<task, deadline of task>
+    ArrayList<HashMap<Integer, Integer>> setOfSets = null;
+
     HashSet<Integer> universe = null;
-    ArrayList<Integer> deadlineAtInstance = null;
-    Integer currentInstance=0;
+    Integer currentTimeInstance = 0;
+    Integer k = 2;
 
     /**
      * Initialize variables
      *
      * @param container
-     * @param deadlineInstances
-     * @param current_instance
+     * @param deadAtTime
+     * @param current_time_instance
      */
-    public SetCoverGreedyWaitTillDeadline(ArrayList<ArrayList> container,
-            ArrayList<Integer> deadlineInstances , Integer current_instance) {
-        if(deadlineInstances.size() != container.size())
-            return;
-        currentInstance = current_instance;
-        setOfSets = new ArrayList<>();
+    public SetCoverGreedyWaitTillDeadline(ArrayList<HashMap<Integer, Integer>> container,
+            Integer current_time_instance) {
+        currentTimeInstance = current_time_instance;
         universe = new HashSet<>();
-
+        setOfSets = new ArrayList<>();
         for (int i = 0; i < container.size(); i++) {
-            ArrayList<Integer> items = container.get(i);
-            if (items != null) {
-                HashSet<Integer> itemSet = new HashSet<Integer>(items);
+            HashMap<Integer, Integer> item = container.get(i);
+            if (item != null) {
+                HashMap<Integer, Integer> itemSet = new HashMap<>(item);
                 setOfSets.add(itemSet);
-                universe.addAll(itemSet);
-                deadlineAtInstance.add(deadlineInstances.get(i));
+                universe.addAll(item.keySet());
+
             }
+
         }
+    }
+
+    /*
+     Check worker contain elemenst will not available at next time
+     */
+    private boolean containElementDeadAtNextTime(HashMap<Integer, Integer> s,
+            int current_time_instance) {
+        if (s.values().contains(current_time_instance)) {
+            return true;
+        }
+
+        return false;
     }
 
     /**
      * Greedy algorithm
      */
     public int minSetCover() {
-        ArrayList<HashSet<Integer>> S = new ArrayList<>();
-        HashSet<Integer> Q = new HashSet<>();
+        ArrayList<HashMap<Integer, Integer>> S = (ArrayList<HashMap<Integer, Integer>>) setOfSets.clone();
+        HashSet<Integer> Q = (HashSet<Integer>) universe.clone();
         HashSet<Integer> C = new HashSet<Integer>();
-        
-        // filter out tasks do not pass deadline in the next time instance
-        for(int i=0;i<setOfSets.size(); i++)
-        {
-            if(Objects.equals(deadlineAtInstance.get(i), currentInstance))
-            {
-                HashSet<Integer> set = setOfSets.get(i);
-                S.add(set);
-                Q.addAll(set);
-                
-            }
-        }
+
         int set_size = S.size();
 
         while (!Q.isEmpty()) {
-            HashSet<Integer> maxSet = null;
+            HashMap<Integer, Integer> maxSet = null;
             int maxElem = 0;
-            for (HashSet<Integer> s : S) {
-				// select the item set that maximize coverage
+            for (HashMap<Integer, Integer> s : S) {
+
+                //
+                // select the item set that maximize coverage
                 // how many elements in s that are not in C
                 int newElem = 0;
-                for (Integer i : s) {
+                for (Integer i : s.keySet()) {
                     if (!C.contains(i)) {
                         newElem++;
                     }
                 }
-                if (newElem > maxElem) {
+                if (newElem > maxElem
+                        && (newElem > k || containElementDeadAtNextTime(s, currentTimeInstance))) // check condition: only select workers that either cover at least K (e.g., k=2,3..)
+                //tasks or cover any task that will not available in the next time instance
+                {
                     maxElem = newElem;
                     maxSet = s;
                 }
             }
 
             S.remove(maxSet);
-            Q.removeAll(maxSet);
-            C.addAll(maxSet);
+            Q.removeAll(maxSet.keySet());
+            C.addAll(maxSet.keySet());
         }
 
         return set_size - S.size();
